5eb83e0b418b9dfc53fc78be8663c94c5cb62aec
Mina Mansour <mansourmina10@gmail.com>
2021-06-23 11:30:03 +0200

    first commit

---
+++ model/schemas.js
@@ -0,0 +1,5 @@
+const { Schema, model } = require('mongoose');
+
+const dog = new Schema({ name: String, born: Date });
+const Dog = model('dog', dog);
+module.exports = { Dog };
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { getDogs } = require('../model/dogs');
+
+const router = express.Router();
+
+router.get(
+  '/welcome',
+  asyncHandler((req, res) => res.send('Willkommen beim Basis Server Pimped!')),
+);
+
+router.get(
+  '/dogs',
+  asyncHandler(async (req, res) => {
+    const result = await getDogs();
+    res.status(200).send(result);
+  }),
+);
+
+module.exports = router;
